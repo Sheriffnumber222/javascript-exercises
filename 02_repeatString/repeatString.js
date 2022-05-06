335acd1ca36bb59711f8d2ec326f66896ade915b
Brett Sheriff <Sheriff222@gmail.com>
2022-05-06 16:46:55 -0700

    Start 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(wordRepeated, numberOfRepeats) {
+    if 
+    let wordRepeatedArray = [wordRepeated];
+    for (i = 0; i < numberOfRepeats-1; i++) {
+        wordRepeatedArray.push(wordRepeated);
+    }
+    wordRepeated = wordRepeatedArray.join(``);
+    return wordRepeated;
 };
 
 // Do not edit below this line
