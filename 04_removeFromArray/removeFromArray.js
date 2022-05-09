53917ba7b2f23ef6cb2755e09b6be510dff8d309
Brett Sheriff <Sheriff222@gmail.com>
2022-05-09 16:03:44 -0700

    Complete 04 first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayInputed, itemToRemove) {
+    for (i = 0; i < arrayInputed.length; i++) {
+        if (arrayInputed[i] == itemToRemove) {
+            arrayInputed.splice(i, 1);
+        }
+    }
+    return arrayInputed.join();
 };
 
 // Do not edit below this line
