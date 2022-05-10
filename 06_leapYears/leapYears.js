9357be3eb296e7e39d889ad9411e03bb5e16c005
Brett Sheriff <Sheriff222@gmail.com>
2022-05-10 12:01:45 -0700

    Complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(yearSubmitted) {
+    if (yearSubmitted % 4 === 0) {
+        if (yearSubmitted % 100 === 0) {
+            if (yearSubmitted % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+        return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
