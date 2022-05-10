7d7a603e1c1f39903d01b8f734996486b7faef65
Brett Sheriff <Sheriff222@gmail.com>
2022-05-09 17:58:14 -0700

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(intOne, intTwo) {
+    let smallNumber = 0;
+    let bigNumber = 0;
+    let numberSumAll = 0;
 
+    if (intOne < 0 || intTwo < 0 || typeof intOne != `number` || typeof intTwo != `number`) {
+        return `ERROR`;
+    }
+    
+    if (intOne < intTwo) {
+        smallNumber = intOne;
+        bigNumber = intTwo;
+    } else {
+        smallNumber = intTwo;
+        bigNumber = intOne;
+    }
+    for (smallNumber; smallNumber <= bigNumber; smallNumber++) {
+        numberSumAll += smallNumber;
+    }
+
+    return numberSumAll
 };
 
 // Do not edit below this line
